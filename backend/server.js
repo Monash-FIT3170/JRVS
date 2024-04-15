5a5fb5ef39152faca58c4dd58e294d0464476687
ae0n1an <max.verhoef0@gmail.com>
2024-04-15 16:50:20 +1000

    feat added first get route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello world!")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.json({message: 'Get goals'})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
