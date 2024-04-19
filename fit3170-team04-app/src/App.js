672768843c13c54e032333339c7574837fe1c8ce
jackson0076 <jngu0076@student.monash.edu>
2024-04-19 21:20:16 +1000

    setup react router to temporarily navigate between pages (so we can access ./quizzes page)

--- fit3170-team04-app/src/App.js
+++ fit3170-team04-app/src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/navbar/Navbar';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/home';
+import Quizzes from './pages/Quiz/quizzes';
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <BrowserRouter>
+      <Routes>
 
-      </header>
-    </div>
+        {/*WHEN WE SETUP MONGODB, WE CAN ROUTE IT BETTER I THINK? */}
+        <Route path = "/" element = {<Home/>}></Route>
+        <Route path = "/quizzes" element = {<Quizzes/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ fit3170-team04-app/src/pages/Quiz/quizzes.js
@@ -0,0 +1,17 @@
+import Navbar from "../../Components/navbar/Navbar";
+export default function Quizzes() {
+  return (
+    <div className="Home">
+      <Navbar></Navbar>
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+      </header>
+
+
+    </div>
+  );
+
+}
