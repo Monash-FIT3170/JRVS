afa67d9ea947a697548bf71b9142b58bb62ac7a2
Thomas Doyle <tdoy0001@student.monash.edu>
2024-05-06 22:43:36 +1000

    Added learning path icons

--- client/src/pages/LearningPathPage.jsx
+++ client/src/pages/LearningPathPage.jsx
@@ -8,8 +8,12 @@ import {
   SkillGroupDataType,
 } from 'beautiful-skill-tree';
 
-// const data = []; //SkillType[]
-const data = [
+import quizIcon from '../assets/images/QuizIcon.png';
+import writtenLessonIcon from '../assets/images/WrittenLessonIcon.png';
+import videoIcon from '../assets/images/VideoIcon.png';
+
+
+const data = [ //SkillType[]
   {
     id: 'hello-world',
     title: 'Hello World',
@@ -17,6 +21,7 @@ const data = [
       content:
         'This node is the top most level, and will be unlocked, and ready to be clicked.',
     },
+    icon: writtenLessonIcon,
     children: [
       {
         id: 'hello-sun',
@@ -25,6 +30,7 @@ const data = [
           content:
             'This is a parent of the top node, and will locked while the parent isn’t in a selected state.',
         },
+        icon: videoIcon,
         children: [],
       },
       {
@@ -34,6 +40,7 @@ const data = [
           content:
             'This is the child of ‘Hello World and the sibling of ‘Hello Sun’. Notice how the app takes care of the layout automatically? That’s why this is called Beautiful Skill Tree and not just ‘Skill Tree’. (Also the npm namespace had already been taken for the latter so (flick hair emoji).',
         },
+        icon: quizIcon,
         children: [],
       },
     ],
@@ -45,15 +52,17 @@ const LearningPathPage = () => {
     <div>
         <LeftSidebar />
         <h2>Learning Path</h2>
+        
         <SkillProvider>
           <SkillTreeGroup>
-            {({ skillCount }) => (
+            {({ skillCount }) => ( //SkillGroupDataType
               <SkillTree
                 treeId="first-tree"
                 title="Skill Tree"
                 data={data}
                 // collapsible
-                description="My first skill tree"
+                //description="My first skill tree"
+                //handleNodeSelect -> Triggers when clicked
               />
             )}
           </SkillTreeGroup>
