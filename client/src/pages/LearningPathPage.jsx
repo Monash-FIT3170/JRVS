dcc480cdecfde41c276f0429748cf6536e0a3b3d
Thomas Doyle <tdoy0001@student.monash.edu>
2024-05-10 22:14:23 +1000

    Added hard-coded lesson path data for MVP1, linked it to learning path page

--- client/src/pages/LearningPathPage.jsx
+++ client/src/pages/LearningPathPage.jsx
@@ -8,46 +8,25 @@ import {
   SkillGroupDataType,
 } from 'beautiful-skill-tree';
 
-import quizIcon from '../assets/images/QuizIcon.png';
-import writtenLessonIcon from '../assets/images/WrittenLessonIcon.png';
-import videoIcon from '../assets/images/VideoIcon.png';
-
-
-const data = [ //SkillType[]
-  {
-    id: 'hello-world',
-    title: 'Hello World',
-    tooltip: {
-      content:
-        'This node is the top most level, and will be unlocked, and ready to be clicked.',
-    },
-    icon: writtenLessonIcon,
-    children: [
-      {
-        id: 'hello-sun',
-        title: 'Hello Sun',
-        tooltip: {
-          content:
-            'This is a parent of the top node, and will locked while the parent isn’t in a selected state.',
-        },
-        icon: videoIcon,
-        children: [],
-      },
-      {
-        id: 'hello-stars',
-        title: 'Hello Stars',
-        tooltip: {
-          content:
-            'This is the child of ‘Hello World and the sibling of ‘Hello Sun’. Notice how the app takes care of the layout automatically? That’s why this is called Beautiful Skill Tree and not just ‘Skill Tree’. (Also the npm namespace had already been taken for the latter so (flick hair emoji).',
-        },
-        icon: quizIcon,
-        children: [],
-      },
-    ],
-  },
-];
+// Import hard-coded learning path data
+import { learningPathData, savedProgressData } from './learningPathData.js';
 
 const LearningPathPage = () => {
+
+  // TODO: Retrieve learning path data from database, rather than it being hard-coded
+  // TODO: Retreive user's progress from database, rather than it being hard-coded
+
+  // Function for handling when a lesson is clicked
+  const handleNodeSelect = (node) => {
+    // ID of lesson is stored in node.key
+    console.log("Node with key '" + node.key + "' selected!")
+
+    // TODO: Popup of some kind? Either find a way to use the tree module or our use our own (Eg use MUI component)
+    // TODO: Navigate to that lesson's page, using the id retrieved here
+
+    // alert("Hello! The lesson selected is " + node.key);
+  }
+
   return (
     <div>
         <LeftSidebar />
@@ -59,14 +38,16 @@ const LearningPathPage = () => {
               <SkillTree
                 treeId="first-tree"
                 title="Skill Tree"
-                data={data}
-                // collapsible
-                //description="My first skill tree"
-                //handleNodeSelect -> Triggers when clicked
+                data={learningPathData} // SkillType
+
+                // Other useful fields (the rest we won't need):
+                savedData={savedProgressData} // To load user progress
+                handleNodeSelect={handleNodeSelect} // To trigger an action when a lesson is clicked
               />
             )}
           </SkillTreeGroup>
         </SkillProvider>;
+
     </div>
   );
 }
