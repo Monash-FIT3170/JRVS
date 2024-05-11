6c907bc38804c9dfc63bd09fa8ea81eca6ec5cd4
Daniel Ding <ddin0018@student.monash.edu>
2024-05-11 15:39:11 +1000

    Finished front-end component of the signup page

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -27,10 +27,43 @@ const RegistrationPage = () => {
             </div>
 
             {/* Form */}
-            <div className='h-screen place-content-evenly ml-48'>
-                <p class="text-6xl font-bold text-ai-blue mb-12">SIGN UP</p>
-                <div>
-                    <input type='text'></input>
+            <div className='h-screen place-content-evenly ml-24'>
+                <p class="text-6xl font-bold text-ai-blue mb-12 text-center">SIGN UP</p>
+                <div class="grid grid-cols-1 gap-6">
+                    <label class="block">
+                        <span class="text-gray-700 mr-48">First name</span>
+                        <span class="text-gray-700">Last name</span>
+                        <div class="flex">
+                            <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mr-12"></input>
+                            <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+                        </div>   
+                    </label>
+
+                    <label class="block">
+                        <span class="text-gray-700">Username</span>
+                        <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+                    </label>
+                    
+                    <label class="block">
+                        <span class="text-gray-700">Email</span>
+                        <input type="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="john@example.com"></input>
+                    </label>
+
+                    <label class="block">
+                        <span class="text-gray-700">School</span>
+                        <input type="search" class="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>                    
+                    </label>
+
+                    <label class="block">
+                        <span class="text-gray-700">Password</span>
+                        <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+                    </label>
+
+                    <label class="block">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Create My Account
+                        </button>                    
+                    </label>
                 </div>
             </div>
         </div>
