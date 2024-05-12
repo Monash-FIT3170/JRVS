3a1bcbfffa33516bdd3cd28bf94c303d99457515
Daniel Ding <ddin0018@student.monash.edu>
2024-05-13 03:37:04 +1000

    user registration works now!

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import avatar from '../assets/images/Avatar.png';
 
 
 
 const RegistrationPage = () => {
+    // Create the hooks for the different inputs
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [userName, setUserName] = useState('')
+    const [email, setEmail] = useState('')
+    const [school, setSchool] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleSubmit = async (e) =>{
+        // stop site from reloading
+        e.preventDefault()
+
+        // create user object to post
+        const user = {username: userName, firstname: firstName, lastname: lastName, email: email, school: school, password: password}
+
+
+        // call a fetch to POST the user
+        const response = await fetch('http://localhost:5000/api/users', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+
+        // receive json
+        const json = await response.json()
+
+        // error check
+        if(!response.ok){
+            console.log(json.error)
+        }
+
+        // if response ok then console log user added
+        if(response.ok){
+            setUserName('')
+            setFirstName('')
+            setLastName('')
+            setEmail('')
+            setSchool('')
+            setPassword('')
+            console.log('new user added', json)
+        }
+    }
 
 
     return (
@@ -27,45 +73,75 @@ const RegistrationPage = () => {
             </div>
 
             {/* Form */}
-            <div className='h-screen place-content-evenly ml-24'>
-                <p class="text-6xl font-bold text-ai-blue mb-12 text-center">SIGN UP</p>
-                <div class="grid grid-cols-1 gap-6">
-                    <label class="block">
-                        <span class="text-gray-700 mr-48">First name</span>
-                        <span class="text-gray-700">Last name</span>
-                        <div class="flex">
-                            <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mr-12"></input>
-                            <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
+            <form className='create' onSubmit={handleSubmit}>
+                <div className='h-screen place-content-evenly ml-24'>
+                    <p class="text-6xl font-bold text-ai-blue mb-12 text-center">SIGN UP</p>
+                    {/* Create the grid layout */}
+                    <div class="grid grid-cols-1 gap-6">
+                        <div class="flex gap-2">
+                            <label>
+                                <span class="text-gray-700 mr">First name</span>
+                                <input type='text' 
+                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                onChange={(e) => setFirstName(e.target.value)}
+                                value={firstName}
+                                ></input>
+                            </label>
+                            <label>
+                                <span class="text-gray-700">Last name</span>
+                                <input type='text' 
+                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                onChange={(e) => setLastName(e.target.value)}
+                                value={lastName}
+                                ></input>
+                            </label>
                         </div>   
-                    </label>
 
-                    <label class="block">
-                        <span class="text-gray-700">Username</span>
-                        <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
-                    </label>
-                    
-                    <label class="block">
-                        <span class="text-gray-700">Email</span>
-                        <input type="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="john@example.com"></input>
-                    </label>
+                        <label class="block">
+                            <span class="text-gray-700">Username</span>
+                            <input type='text' 
+                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            onChange={(e) => setUserName(e.target.value)}
+                            value={userName}
+                            ></input>
+                        </label>
+                        
+                        <label class="block">
+                            <span class="text-gray-700">Email</span>
+                            <input type="email" 
+                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
+                            placeholder="john@example.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            ></input>
+                        </label>
 
-                    <label class="block">
-                        <span class="text-gray-700">School</span>
-                        <input type="search" class="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>                    
-                    </label>
+                        <label class="block">
+                            <span class="text-gray-700">School</span>
+                            <input type="search" 
+                            class="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            onChange={(e) => setSchool(e.target.value)}
+                            value={school}
+                            ></input>                    
+                        </label>
 
-                    <label class="block">
-                        <span class="text-gray-700">Password</span>
-                        <input type='text' class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></input>
-                    </label>
+                        <label class="block">
+                            <span class="text-gray-700">Password</span>
+                            <input type='text' 
+                            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            onChange={(e)=> setPassword(e.target.value)}
+                            value={password}
+                            ></input>
+                        </label>
 
-                    <label class="block">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                            Create My Account
-                        </button>                    
-                    </label>
+                        <label class="block">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                Create My Account
+                            </button>                    
+                        </label>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
         
     )
--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/badges', require('./routes/badgeRoutes'))
 app.use('/api/quizzes', require('./routes/quizRoutes'))
 app.use('/api/lessons', require('./routes/lessonRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
