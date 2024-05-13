19cd82108b8f75fd929f70af9d7ed90580ba5a02
Maily Butel <mbut0011@student.monash.edu>
2024-05-13 14:48:58 +1000

    Add onClick link to lessonPath

--- client/src/pages/UnitsPage.jsx
+++ client/src/pages/UnitsPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
+import { useNavigate } from 'react-router-dom'
+
 
 
 import { useApi } from '../context/ApiProvider';
@@ -23,22 +25,30 @@ const UnitsPage = () => {
   //     fetchData();
   //   }, [getData])
 
+  const navigate = useNavigate()
+
+  const routeChange = () => {
+    // let path = "/lessonPath";
+    let path = "/lessonPath"
+    navigate(path);
+  } 
+
   return (
     // Top NavBar
     //imageColour="#66CC66"
     <div>
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
       <Grid container spacing={3} padding={10}>
-        <Grid item xs={3}>
+        <Grid item xs={3} onClick={routeChange}>
           <UnitCard title="Introduction to AI" progress={70} imageColour="#66CC66" icon="code"></UnitCard>
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={3} onClick={routeChange}>
           <UnitCard title="Recognising AI" progress={20} imageColour="#A366FF" icon="search"></UnitCard>
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={3} onClick={routeChange}>
           <UnitCard title="Understanding Intelligence" progress={50} imageColour="#FFC93C" icon="tips_and_updates"></UnitCard>
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={3} onClick={routeChange}>
           <UnitCard title="Types of AI" progress={100} imageColour="#4885FF" icon="memory"></UnitCard>
         </Grid>
       </Grid>
