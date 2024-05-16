e1179df3bfbae4fde04189ce0f5e5c6aa567dcfd
jackson0076 <jngu0076@student.monash.edu>
2024-05-16 21:25:19 +1000

    update

--- client/src/components/quizComponents/Reorder.jsx
+++ client/src/components/quizComponents/Reorder.jsx
@@ -14,7 +14,6 @@ export default function Reorder({ question, index, setSelection, userValues }) {
     let order = userValues[question.questionText] || question.options
 
     //We can set the selection first before we do anything just in case
-    
     const onDrop = ({ removedIndex, addedIndex }) => {
         //We remove the item with splice, then add the item back to the new index
         const [removedOption] = order.splice(removedIndex, 1);
@@ -22,23 +21,22 @@ export default function Reorder({ question, index, setSelection, userValues }) {
         setSelection(question.questionText, order);
     }
 
-    const mappedIndex = (order).map((item,idx) => (
+    const mappedIndex = (order).map((item, idx) => (
         <Draggable key={idx}>
-        <ListItem sx={{
-            my: '8px',
-            ml: '10px',
-            display: 'flex',
-            alignItems: 'center',
-            fontFamily: '"Roboto-Regular", Helvetica',
-        }}  >
-           <ListItemText
+            <ListItem sx={{
+                my: '8px',
+                ml: '10px',
+                display: 'flex',
+                alignItems: 'center',
+                fontFamily: '"Roboto-Regular", Helvetica',
+            }}  >
+                <ListItemText
                     primary={idx + 1}
-                    primaryTypographyProps={{fontSize: '18px', fontWeight: 500}} />
-        </ListItem>
-    </Draggable>
-       
-    ))
+                    primaryTypographyProps={{ fontSize: '18px', fontWeight: 500 }} />
+            </ListItem>
+        </Draggable>
 
+    ))
 
     const mappedOptions = (order).map((item) => (
         <Draggable key={item}>
@@ -54,8 +52,8 @@ export default function Reorder({ question, index, setSelection, userValues }) {
                 width: '50%'
 
             }}  >
-               
-                <ListItemText primary={item} sx={{fontSize: 700}} />
+
+                <ListItemText primary={item} sx={{ fontSize: 700 }} />
 
 
             </ListItem>
@@ -70,21 +68,20 @@ export default function Reorder({ question, index, setSelection, userValues }) {
                         <Typography sx={{ display: "inline", color: '#3ca3ee', fontSize: '30px', lineHeight: '60px', fontWeight: 700, mr: '5px' }}>Question {index + 1}</Typography>
                         <Typography sx={{ display: "inline", color: '#000000', fontFamily: '"Roboto-Regular", Helvetica', fontSize: '16px', lineHeight: '32px', }}></Typography>{/*Needs fixing to display total question fix another time */}
                         <Typography sx={{ display: 'block', color: '#000000', fontFamily: '"Roboto-Regular", Helvetica', fontSize: '16px', my: '20px' }}>{question.questionText}</Typography>
-                        
+
                         <Grid container>
-                        <Grid xs={1}>
-                        <Container  lockAxis="x,y">
-                        {mappedIndex}
-                            </Container>
-                                
-                        </Grid>
+                            <Grid xs={1}>
+                                <Container lockAxis="x,y">
+                                    {mappedIndex}
+                                </Container>
+                            </Grid>
 
 
-                        <Grid  xs={11}>
-                            <Container lockAxis="y" onDrop={onDrop}>
-                                {mappedOptions}
-                            </Container>
-                        </Grid>
+                            <Grid xs={11}>
+                                <Container lockAxis="y" onDrop={onDrop}>
+                                    {mappedOptions}
+                                </Container>
+                            </Grid>
 
                         </Grid>
                     </Grid>
--- client/src/components/quizComponents/Submitted.jsx
+++ client/src/components/quizComponents/Submitted.jsx
@@ -47,12 +47,8 @@ export default function Submitted({ score, totalScore }) {
                     </div>
                 </Box>
 
-
-
             </Grid>
             <Grid item xs={12} display="flex" justifyContent="center">
-
-
                 <Button variant="contained" className="button-font"
                     sx={{ ':hover': { backgroundColor: '#E6B635' }, padding: '14px', mt: '20px', borderRadius: '15px', backgroundColor: '#FFC93C', fontSize: '20px', fontWeight: 700, fontFamily: '"Roboto-Bold", Helvetica' }}>
                     Return to learning path
