0ebad5e09125634b6ca3d25b35e7548ee036e132
Max Verhoef <mver0017@student.monash.edu>
2024-05-17 12:51:43 +1000

    feat added register and login routes

---
+++ server/routes/auth.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Register route
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).send('User created');
+  } catch (error) {
+    res.status(400).send('Error creating user');
+  }
+});
+
+// Login route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).send('Invalid username or password');
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).send('Invalid username or password');
+    
+    const token = jwt.sign({ id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
