cea1980abd2b7e027883c527d40a668d7d73b0ef
Caleb Smith <csmi0037@student.monash.edu>
2024-05-18 06:05:11 +1000

    added avatar to profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ import avatar from '../assets/images/Avatar.png';
 import MenuBar from '../components/MenuBar';
 import { Button } from "@mui/material";
 import { useApi } from '../context/ApiProvider';
+import Avatar from '../components/characterCustomization/Avatar';
 
 const ProfilePage = () => {
   const { getData, postData } = useApi();
@@ -12,6 +13,7 @@ const ProfilePage = () => {
   const [isBadgeLoading, setIsBadgeLoading] = useState(true);
   const [user, setUser] = useState({ username: '', points: 0 });
   const [isUserLoading, setIsUserLoading] = useState(true);
+  const [avatar, setAvatar] = useState({avatar: '_default.png', border: '_default.png', background: '_default.png'})
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +32,7 @@ const ProfilePage = () => {
         const userData = await getData(`api/users/id/${res.decoded.id}`);
         console.log(userData);
         setUser({ username: userData.username, points: userData.points });
+        setAvatar({avatar: userData.avatar, border: userData.border, background: userData.background})
         setIsUserLoading(false);
       } catch (error) {
         console.log(error);
@@ -42,16 +45,17 @@ const ProfilePage = () => {
   }, [getData])
   return (
     <div>
-      <MenuBar coins={user.points}/>
-      <div className='ml-48'>
+      <MenuBar coins={user.points} title={'Welcome User'}/>
+      <div className='ml-48 ' >
         <Grid container spacing={2}>
-          <Grid xs={12} style={{ padding: '40px'}}>
+          {/* <Grid xs={12} style={{ padding: '40px'}}>
             <h2 className='russo-one-regular text-6xl'>Welcome User</h2>
-          </Grid>
+          </Grid> */}
           <Grid xs={4} style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <div style={{marginBottom: '20px', flexGrow: '1', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            {/* <div style={{marginBottom: '20px', flexGrow: '1', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
               <img src={avatar} alt='avatar icon'></img>
-            </div>
+            </div> */}
+            <Avatar avatar={avatar.avatar} background={avatar.background} border={avatar.border}/>
             <Button href="/customize">
             <div style={{ border: '2px solid #2196f3', padding: '20px', marginBottom: '40px', flexGrow: '1', width: '60%', textAlign:'center', borderRadius: '20px', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' , backgroundColor: '#3CA3EE'}}>
               <h2 href="/customize" className='russo-one-regular text-3xl'>customize avatar</h2>
