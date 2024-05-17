b739c2f88df5266ec2bbcf758a2e06316a101609
Max Verhoef <mver0017@student.monash.edu>
2024-05-17 14:41:17 +1000

    added logging in request to the login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,12 +19,13 @@ const LoginPage = () => {
         e.preventDefault();
         try {
           const res = await postData('api/auth/login', { username, password });
-          localStorage.setItem('token', res.data.token);
+          console.log(res)
+          localStorage.setItem('token', res.token);
           navigate('/');
         } catch (error) {
           console.error('Login failed', error);
         }
-      };
+    };
 
 
     return (
@@ -49,7 +50,7 @@ const LoginPage = () => {
 
                         <div>
                             <span className="text-gray-700">Password</span>
-                            <input type='text' 
+                            <input type='password' 
                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             onChange={(e)=> setPassword(e.target.value)}
                             value={password}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const { Double } = require('mongodb')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
     {
