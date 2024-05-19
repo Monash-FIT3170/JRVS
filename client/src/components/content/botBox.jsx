3c1e93a38d1949c9205de66a441a168022441fbb
Brendan Nguyen <bngu0037@student.monash.edu>
2024-05-19 18:08:28 +1000

    hotfix: added spline model to lesson page

--- client/src/components/content/botBox.jsx
+++ client/src/components/content/botBox.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
+import Spline from '@splinetool/react-spline'
 
 export default function BotBox() {
 
     return (
         <Box borderRadius={5} p={5} sx={{backgroundColor: '#E6EBEF', width: '350px', height: '400px', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
+            <Spline style={{width: "100%", height: '100%', alignContent: 'center'}} scene="https://prod.spline.design/XKEgqYXVlVb78tGc/scene.splinecode"/>
         </Box>
     )
 }
