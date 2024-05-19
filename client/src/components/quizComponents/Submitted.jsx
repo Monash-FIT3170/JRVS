e0b6caaef739c5525e7aaae618e41cc930f010e2
Brendan Nguyen <bngu0037@student.monash.edu>
2024-05-19 17:32:37 +1000

    fixed quiz button not linking to learning path

--- client/src/components/quizComponents/Submitted.jsx
+++ client/src/components/quizComponents/Submitted.jsx
@@ -50,7 +50,7 @@ export default function Submitted({ score, totalScore }) {
 
             </Grid>
             <Grid item xs={12} display="flex" justifyContent="center">
-                <Button variant="contained" className="button-font"
+                <Button href='/learningPath' variant="contained" className="button-font"
                     sx={{ ':hover': { backgroundColor: '#E6B635' }, padding: '14px', mt: '20px', borderRadius: '15px', backgroundColor: '#FFC93C', fontSize: '20px', fontWeight: 700, fontFamily: '"Roboto-Bold", Helvetica' }}>
                     Return to learning path
                 </Button>
