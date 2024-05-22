75a2b7c7aab1b13c532d06acc7734f606d27774f
Max Verhoef <mver0017@student.monash.edu>
2024-05-22 13:57:42 +1000

    feat added ability to pass parameters into bot box with default values set

--- client/src/components/content/botBox.jsx
+++ client/src/components/content/botBox.jsx
@@ -1,11 +1,27 @@
 import { Box } from "@mui/material";
-import Spline from '@splinetool/react-spline'
+import Spline from '@splinetool/react-spline';
 
-export default function BotBox() {
-
+export default function BotBox({
+    backgroundColor = '#E6EBEF',
+    width = '350px',
+    height = '400px',
+    boxShadow = '0px 4px 4px rgba(0, 0, 0, 0.25)',
+}) {
     return (
-        <Box borderRadius={5} p={5} sx={{backgroundColor: '#E6EBEF', width: '350px', height: '400px', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
-            <Spline style={{width: "100%", height: '100%', alignContent: 'center'}} scene="https://prod.spline.design/XKEgqYXVlVb78tGc/scene.splinecode"/>
+        <Box
+            borderRadius={5}
+            p={5}
+            sx={{
+                backgroundColor: backgroundColor,
+                width: width,
+                height: height,
+                boxShadow: boxShadow
+            }}
+        >
+            <Spline
+                style={{ width: "100%", height: '100%', alignContent: 'center' }}
+                scene='https://prod.spline.design/XKEgqYXVlVb78tGc/scene.splinecode'
+            />
         </Box>
-    )
+    );
 }
