e6bfbc1afb7d34e9eac83dd553d88c3be4903186
Max Verhoef <mver0017@student.monash.edu>
2024-07-29 00:44:41 +1000

    feat: updated leaderboard ui so scrolling works correctly

--- client/src/pages/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage.jsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import MenuBar from "../components/MenuBar";
+import { useApi } from '../context/ApiProvider';
 
-const Leaderboard = () => {
+const LeaderboardPage = () => {
+    const { getData } = useApi();
     const [timePeriod, setTimePeriod] = useState('all-time');
     const [userGroup, setUserGroup] = useState('all');
     const [leaderboardData, setLeaderboardData] = useState([]);
 
     useEffect(() => {
-        // Fetch leaderboard data based on selected filters
         const fetchLeaderboardData = async () => {
-            // Here you would make an API request to your backend to fetch leaderboard data
-            // based on the selected time period and user group
-            // For example:
-            // const response = await fetch(`/api/leaderboard?timePeriod=${timePeriod}&userGroup=${userGroup}`);
-            // const data = await response.json();
-            // setLeaderboardData(data);
+            try {
+                const leaderboardResponse = await getData(`api/xp/leaderboard?timePeriod=${timePeriod}&userGroup=${userGroup}`);
+                const usersResponse = await getData('api/users');
 
-            // Mock data for demonstration
-            const mockData = [
-                { username: 'user1', xp: 150 },
-                { username: 'user2', xp: 120 },
-                { username: 'user3', xp: 100 },
-            ];
-            setLeaderboardData(mockData);
+                const userXpMap = {};
+                leaderboardResponse.forEach(item => {
+                    userXpMap[item.userId] = item.totalXP;
+                });
+
+                const completeLeaderboardData = usersResponse.map(user => ({
+                    username: user.username,
+                    xp: userXpMap[user._id] || 0,
+                }));
+
+                setLeaderboardData(completeLeaderboardData.sort((a, b) => b.xp - a.xp));
+            } catch (error) {
+                console.error(error);
+            }
         };
 
         fetchLeaderboardData();
-    }, [timePeriod, userGroup]);
+    }, [timePeriod, userGroup, getData]);
 
     return (
-        <div className='App-page'>
-            <MenuBar/>
-            <div className="flex flex-col items-center justify-start">
-                <h2 style={{color: 'white', font: 'Roboto', fontWeight: '700', fontSize: '60px'}}>LEADERBOARD</h2>
+        <div className='App-page' style={{ height: '100vh' }}>
+            <MenuBar />
+            <div className="flex flex-col items-center justify-start h-3/4">
+                <h2 style={{ color: 'white', fontFamily: 'Roboto', fontWeight: '700', fontSize: '60px' }}>LEADERBOARD</h2>
                 <div style={{
                     border: '1px solid black',
                     padding: '20px',
@@ -42,13 +47,12 @@ const Leaderboard = () => {
                     borderRadius: '20px',
                     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
                     backgroundColor: 'white',
-                    maxHeight: 'calc(100vh - 200px)', // Adjust based on MenuBar height
-                    overflowY: 'auto'
+                    overflowY: 'hidden'
                 }}>
                     <div className="flex justify-center mb-4">
                         <h2 style={{
                             color: 'black',
-                            font: 'Roboto',
+                            fontFamily: 'Roboto',
                             fontWeight: '500',
                             fontSize: '20px',
                             padding: '5px'
@@ -58,14 +62,14 @@ const Leaderboard = () => {
                             value={timePeriod}
                             onChange={(e) => setTimePeriod(e.target.value)}
                         >
-                            <option value="day">Last Day</option>
-                            <option value="week">Last Week</option>
-                            <option value="month">Last Month</option>
                             <option value="all-time">All Time</option>
+                            <option value="month">Last Month</option>
+                            <option value="week">Last Week</option>
+                            <option value="day">Today</option>
                         </select>
                         <h2 style={{
                             color: 'black',
-                            font: 'Roboto',
+                            fontFamily: 'Roboto',
                             fontWeight: '500',
                             fontSize: '20px',
                             padding: '5px',
@@ -81,26 +85,35 @@ const Leaderboard = () => {
                             <option value="class">Your Class</option>
                         </select>
                     </div>
-                    <table className="min-w-full divide-y divide-gray-200">
-                        <thead>
-                        <tr>
-                            <th className="py-2">Username</th>
-                            <th className="py-2">XP</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {leaderboardData.map((user, index) => (
-                            <tr key={index} className="border-b">
-                                <td className="py-2">{user.username}</td>
-                                <td className="py-2">{user.xp}</td>
+                    <div style={{
+                        textAlign: 'center',
+                        backgroundColor: 'white',
+                        maxHeight: '100%',
+                        overflowY: 'scroll'
+                    }}>
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead>
+                            <tr>
+                                <th className="py-2">Rank</th>
+                                <th className="py-2">Username</th>
+                                <th className="py-2">XP</th>
                             </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            {leaderboardData.map((user, index) => (
+                                <tr key={index} className="border-b">
+                                    <td className="py-2">{index + 1}</td>
+                                    <td className="py-2">{user.username}</td>
+                                    <td className="py-2">{user.xp}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Leaderboard;
+export default LeaderboardPage;
