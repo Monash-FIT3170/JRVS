720cabc7998f2a24aa238891e76bcb5325284101
Thomas Doyle <tdoy0001@student.monash.edu>
2024-07-29 20:34:31 +1000

    Created basic buttons for tree node modification functionality

--- client/src/components/UnitPopup.jsx
+++ client/src/components/UnitPopup.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
 
-const UnitPopup = ({ isOpen, node, onClose }) => {
+const UnitPopup = ({ isOpen, node, onClose, onInsert, onAppend, onEdit, onDelete, isAdmin }) => {
     const handleEnterLesson = () => {
         window.location.href = `http://localhost:3000/${node.type}/${node.id}`;
     };
 
     return (
         <Dialog open={isOpen} onClose={onClose}>
-            <DialogTitle>Node Selected {node && node.title}</DialogTitle>
+            <DialogTitle>
+                {node && node.title}
+            </DialogTitle>
             <DialogContent>
                 <DialogContentText>
-                    {node && `You selected a ${node.type} nodess with id: ${node.id}`}
+                    {node && node.content}
                 </DialogContentText> 
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose} color="primary">
-                    Close
-                </Button>
-                <Button onClick={handleEnterLesson
-            
-                } color="primary">
-                    Go to {node && node.type}
-                </Button>
+                <Button onClick={onClose} color="primary">Close</Button>
+                <Button onClick={handleEnterLesson} color="primary">Go to {node && node.type}</Button>
             </DialogActions>
+            {
+                isAdmin &&
+                <DialogActions>
+                    <Button onClick={onInsert} color="primary">Insert child</Button>
+                    <Button onClick={onAppend} color="primary">Append child</Button>
+                    <Button onClick={onEdit} color="primary">Edit</Button>
+                    <Button onClick={onDelete} color="primary">Delete</Button>
+                </DialogActions>
+            }
+            
         </Dialog>
     );
 };
