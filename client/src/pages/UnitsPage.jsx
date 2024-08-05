698a38c94329a5f824abe918fb3199baf98631c8
Jas <jbha0006@student.monash.edu>
2024-08-05 10:55:09 +1000

    dynamically retriving unit progress

--- client/src/pages/UnitsPage.jsx
+++ client/src/pages/UnitsPage.jsx
@@ -7,72 +7,85 @@ import UnitCard from "../components/UnitCard";
 import MenuBar from "../components/MenuBar";
 
 const UnitsPage = () => {
-    const { getData } = useApi();
-    const [units, setUnits] = useState(undefined);
-    const [isUnitLoading, setIsUnitLoading] = useState(true); // set loading spinner
+  const { getData, postData } = useApi();
+  const [units, setUnits] = useState();
+  const [userData, setUserData] = useState();
+  const [isUnitLoading, setIsUnitLoading] = useState(true);
+  const [isUserDataLoading, setIsUserDataLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const unitsResponse = await getData("api/units");
-                setUnits(unitsResponse);
-                setIsUnitLoading(false);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchData();
-    }, [getData]);
+  const isLoading = isUnitLoading || isUserDataLoading;
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const unitsResponse = await getData("api/units");
+        setUnits(unitsResponse);
+        setIsUnitLoading(false);
 
-    const routeChange = (unitId) => {
-      console.log(unitId);
-      let path = `/learningPath/${unitId}`;
-        navigate(path);
+        const token = localStorage.getItem("token");
+        const res = await postData("api/auth/current", { token });
+        const userData = await getData(`api/users/id/${res.decoded.id}`);
+        setUserData(userData);
+        setIsUserDataLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    fetchData();
+  }, [getData]);
 
-    return (
-        <div>
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            ></link>
-            <MenuBar
-                title="Unit Overview"
-                subtitle="Get ready to learn more about AI today"
-            ></MenuBar>
+  const navigate = useNavigate();
+
+  const routeChange = (unitId) => {
+    let path = `/learningPath/${unitId}`;
+    navigate(path);
+  };
+
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      ></link>
+      <MenuBar
+        title="Unit Overview"
+        subtitle="Get ready to learn more about AI today"
+      ></MenuBar>
+      <Grid
+        container
+        rowSpacing={6}
+        columnSpacing={5}
+        padding={10}
+        backgroundColor="white"
+      >
+        {isLoading ? (
+          <div className="spinner"></div>
+        ) : (
+          units.map((unit) => (
             <Grid
-                container
-                rowSpacing={6}
-                columnSpacing={5}
-                padding={10}
-                backgroundColor="white"
+              item
+              xs={6}
+              sm={6}
+              md={4}
+              lg={3}
+              onClick={() => routeChange(unit._id)}
             >
-                {isUnitLoading ? (
-                    <div className="spinner"></div>
-                ) : (
-                    units.map((unit) => (
-                        <Grid
-                            item
-                            xs={6}
-                            sm={6}
-                            md={4}
-                            lg={3}
-                            onClick={() => routeChange(unit._id)}
-                        >
-                            <UnitCard
-                                title={unit.title}
-                                progress={70} // TODO: placeholder
-                                imageColour={unit.colour}
-                                icon={unit.icon}
-                            ></UnitCard>
-                        </Grid>
-                    ))
-                )}
+              <UnitCard
+                title={unit.title}
+                progress={
+                  ((userData?.assignedUnits.find(
+                    (userUnit) => userUnit.unitId == unit._id
+                  )?.lessonsCompleted || 0) / unit.numberOfLessons)*100
+                }
+                imageColour={unit.colour}
+                icon={unit.icon}
+              ></UnitCard>
             </Grid>
-        </div>
-    );
+          ))
+        )}
+      </Grid>
+    </div>
+  );
 };
 
 export default UnitsPage;
