d84e7cd59be43371815c51e39ceda722a7977ec7
Max Verhoef <mver0017@student.monash.edu>
2024-08-05 11:23:49 +1000

    feat: fixed up ui of leaderboard page

--- client/src/pages/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MenuBar from "../components/MenuBar";
 import { useApi } from '../context/ApiProvider';
+import Grid from "@mui/material/Unstable_Grid2";
 
 const LeaderboardPage = () => {
     const { getData, postData } = useApi();
@@ -46,86 +47,93 @@ const LeaderboardPage = () => {
     }, [timePeriod, userGroup, getData]);
 
     return (
-        <div className='App-page' style={{ height: '100vh' }}>
+        <div className='App-page' style={{ height:'100vh' }}>
             <MenuBar />
-            <div className="flex flex-col items-center justify-start h-3/4">
-                <h2 style={{ color: 'white', fontFamily: 'Roboto', fontWeight: '700', fontSize: '60px' }}>LEADERBOARD</h2>
-                <div style={{
-                    border: '1px solid black',
-                    padding: '20px',
-                    marginBottom: '20px',
-                    width: '1000px',
-                    textAlign: 'center',
-                    borderRadius: '20px',
-                    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-                    backgroundColor: 'white',
-                    overflowY: 'hidden'
-                }}>
-                    <div className="flex justify-center mb-4">
-                        <h2 style={{
-                            color: 'black',
-                            fontFamily: 'Roboto',
-                            fontWeight: '500',
-                            fontSize: '20px',
-                            padding: '5px'
-                        }}>Time Period:</h2>
-                        <select
-                            className="border p-2 rounded w-32"
-                            value={timePeriod}
-                            onChange={(e) => setTimePeriod(e.target.value)}
-                        >
-                            <option value="all-time">All Time</option>
-                            <option value="month">Last Month</option>
-                            <option value="week">Last Week</option>
-                            <option value="day">Today</option>
-                        </select>
-                        <h2 style={{
-                            color: 'black',
-                            fontFamily: 'Roboto',
-                            fontWeight: '500',
-                            fontSize: '20px',
-                            padding: '5px',
-                            paddingLeft: '20px'
-                        }}>Users:</h2>
-                        <select
-                            className="border p-2 rounded w-32"
-                            value={userGroup}
-                            onChange={(e) => setUserGroup(e.target.value)}
-                        >
-                            <option value="all">All Users</option>
-                            <option value="school">Your School</option>
-                            <option value="class">Your Class</option>
-                        </select>
-                    </div>
+            <Grid container spacing={2} style={{ padding: '0 30px 0 20px'}}>
+                <Grid xs={12} style={{ padding: '0 0 10px 40px', textAlign: 'center'}}>
+                    <h2 style={{ color: 'white', font: 'Roboto', fontWeight: '700', fontSize: '60px' }}>LEADERBOARD</h2>
+                </Grid>
+                <Grid xs={0} md={3}></Grid>
+                <Grid xs={12} md={6} style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
                     <div style={{
+                        border: '1px solid black',
+                        padding: '20px',
+                        marginBottom: '20px',
                         textAlign: 'center',
+                        borderRadius: '20px',
+                        height: '65vh',
+                        width: '100%',
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
                         backgroundColor: 'white',
-                        maxHeight: '100%',
-                        overflowY: 'scroll'
+                        overflowY: 'hidden'
                     }}>
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead>
-                            <tr>
-                                <th className="py-2">Rank</th>
-                                <th className="py-2">Username</th>
-                                <th className="py-2">School</th>
-                                <th className="py-2">XP</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {leaderboardData.map((user, index) => (
-                                <tr key={index} className="border-b">
-                                    <td className="py-2">{index + 1}</td>
-                                    <td className="py-2">{user.username}</td>
-                                    <td className="py-2">{user.school}</td>
-                                    <td className="py-2">{user.totalXP}</td>
+                        <div className="flex justify-center mb-4">
+                            <h2 style={{
+                                color: 'black',
+                                fontFamily: 'Roboto',
+                                fontWeight: '500',
+                                fontSize: '20px',
+                                padding: '5px'
+                            }}>Time Period:</h2>
+                            <select
+                                className="border p-2 rounded w-32"
+                                value={timePeriod}
+                                onChange={(e) => setTimePeriod(e.target.value)}
+                            >
+                                <option value="all-time">All Time</option>
+                                <option value="month">Last Month</option>
+                                <option value="week">Last Week</option>
+                                <option value="day">Today</option>
+                            </select>
+                            <h2 style={{
+                                color: 'black',
+                                fontFamily: 'Roboto',
+                                fontWeight: '500',
+                                fontSize: '20px',
+                                padding: '5px',
+                                paddingLeft: '20px'
+                            }}>Users:</h2>
+                            <select
+                                className="border p-2 rounded w-32"
+                                value={userGroup}
+                                onChange={(e) => setUserGroup(e.target.value)}
+                            >
+                                <option value="all">All Users</option>
+                                <option value="school">Your School</option>
+                                <option value="class">Your Class</option>
+                            </select>
+                        </div>
+                        <div style={{
+                            textAlign: 'center',
+                            backgroundColor: 'white',
+                            maxHeight: '100%',
+                            overflowY: 'scroll'
+                        }}>
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead>
+                                <tr>
+                                    <th className="py-2">Rank</th>
+                                    <th className="py-2">Username</th>
+                                    <th className="py-2">School</th>
+                                    <th className="py-2">XP</th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                {leaderboardData.map((user, index) => (
+                                    <tr key={index} className="border-b">
+                                        <td className="py-2">{index + 1}</td>
+                                        <td className="py-2">{user.username}</td>
+                                        <td className="py-2">{user.school}</td>
+                                        <td className="py-2">{user.totalXP}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </Grid>
+                <Grid xs={0} md={3} ></Grid>
+            </Grid>
         </div>
     );
 };
