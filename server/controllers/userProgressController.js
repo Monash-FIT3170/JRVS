b61ac5717108ca5cb283e68db6f43fdbd966dbbd
Maily Butel <mbut0011@student.monash.edu>
2024-08-10 14:09:32 +1000

    update user unit progress

--- server/controllers/userProgressController.js
+++ server/controllers/userProgressController.js
@@ -16,9 +16,25 @@ const getuserUnitProgress = asyncHandler (async (req, res) => {
     }
 })
 
+const updateuserUnitProgress = asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const unitId = req.params.unitId;
+    const { completedLessons } = req.body;
 
+    const userUnitProgress = await userUnitProgressModel.findOne({ userId: userId, unitId: unitId });
+
+    if (!userUnitProgress) {
+        res.status(404).json({message: "User's unit progress not found"});
+    } else {
+        userUnitProgress.completedLessons = completedLessons;
+        await userUnitProgress.save();
+        res.status(200).json(userUnitProgress);
+    }
+
+})
 
 
 module.exports = {
-    getUserUnitProgress
+    getUserUnitProgress,
+    updateUserUnitProgress
 }
