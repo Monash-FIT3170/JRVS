dd65d9c1c26d45768edf13efea5b300535e3f251
Maily Butel <mbut0011@student.monash.edu>
2024-08-10 14:08:58 +1000

    get user unit progress

--- server/controllers/userProgressController.js
+++ server/controllers/userProgressController.js
@@ -1,26 +1,24 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
-const Unit = require('../models/unitModel');
 
-// Get the number of completed lessons and total lessons in a unit
-const getUserUnitProgress = asyncHandler(async (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const userUnitProgressModel = require('../models/userUnitProgressModel')
+
+
+const getuserUnitProgress = asyncHandler (async (req, res) => {
     const userId = req.params.userId;
     const unitId = req.params.unitId;
+    const userUnitProgress = await userUnitProgressModel.findOne({ userId: userId, unitId: unitId });
 
-    const user = await User.findById(userId).populate('assignedUnits');
-    const unit = await Unit.findById(unitId);
-
-    if (!user || !unit) {
-        res.status(404).json({ message: 'User or Unit not found' });
+    if (!userUnitProgress) {
+        res.status(404).json({message: "User's unit progress not found"});
     } else {
-        const userProgress = user.assignedUnits.find(u => u._id.toString() === unitId);
-        const completedLessons = userProgress ? userProgress.completedLessons.length : 0;
-        const totalLessons = unit.lessons.length;
-
-        res.status(200).json({ completedLessons, totalLessons });
+        res.status(200).json(userUnitProgress);
     }
-});
+})
+
+
+
 
 module.exports = {
-    getUserUnitProgress,
-};
+    getUserUnitProgress
+}
