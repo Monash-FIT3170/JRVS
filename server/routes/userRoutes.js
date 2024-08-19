2092659f5c6de3c8b1660b538d6840555995da3f
Vivian <vpha0021@student.monash.edu>
2024-08-19 20:55:29 +1000

    Adding change password functionality

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authMiddleware');  // Import the authenticate middleware
-const { createUser, updatePoints, getUserByUsername, getUserById, updateAvatar, updateUnlocked, getAllUsers, updateDetails } = require('../controllers/userController');
+const { createUser, updatePoints, getUserByUsername, getUserById, updateAvatar, updateUnlocked, getAllUsers, updateDetails, updatePassword } = require('../controllers/userController');
 
 // Route to update user points
 router.post('/', createUser)
@@ -9,6 +9,7 @@ router.post('/updatePoints', authenticate, updatePoints);
 router.post('/updateAvatar', updateAvatar);
 router.post('/updateUnlocked', updateUnlocked);  
 router.post('/updateDetails', updateDetails);
+router.post('/updatePassword', updatePassword);
 router.get('/:username', getUserByUsername);
 router.get('/id/:id', getUserById);
 router.get('/', getAllUsers);
