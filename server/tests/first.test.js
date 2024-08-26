0fdf8d04273100178ef11fa9480eae504a321818
Daniel Ding <ddin0018@student.monash.edu>
2024-08-26 11:25:49 +1000

    updating faker???

--- server/tests/first.test.js
+++ server/tests/first.test.js
@@ -1,10 +1,8 @@
+import { faker } from "@faker-js/faker";
 const { MongoClient } = require("mongodb");
-const { faker } = require("@faker-js/faker");
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 const request = require("supertest");
 // const app = require("../server")
-
-
 
 jest.setTimeout(30000);
 
@@ -12,68 +10,69 @@ jest.setTimeout(30000);
 // Init mongo client
 const client = new MongoClient(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 describe("Database Tests", () => {
-    let usersCollection;
-  
-    // Before testing connect to db
-    beforeAll(async () => {
-      try {
-        await client.connect();
-        const db = client.db("test");
-        usersCollection = db.collection("users");
-      } catch (err) {
-        console.error("Error connecting to the database:", err);
-      }
-    });
-  
-    // test creating users
-    test("Test CREATE", async () => {
-      let newUsers = [];
-      let total_users_to_add = 3;
-  
-      for (let i = 0; i < total_users_to_add; i++) {
-        newUsers.push({
-          firstname: faker.person.firstName(),
-          lastname: faker.person.lastName(),
-          email: faker.internet.email(),
-          username: faker.internet.userName()
-        });
-      }
-  
-      const result = await usersCollection.insertMany(newUsers);
-      expect(result.insertedCount).toBe(total_users_to_add);
-    }, 30000);
+  let usersCollection;
 
+  // Before testing connect to db
+  beforeAll(async () => {
+    try {
+      await client.connect();
+      const db = client.db("test");
+      usersCollection = db.collection("users");
+    } catch (err) {
+      console.error("Error connecting to the database:", err);
+    }
+  });
 
-    // test reading users
-    test("Test READ", async () => {
-      let sampleUser = { 
-        firstname: faker.person.firstName(), 
+  // test creating users
+  test("Test CREATE", async () => {
+    let newUsers = [];
+    let total_users_to_add = 3;
+
+    for (let i = 0; i < total_users_to_add; i++) {
+      newUsers.push({
+        firstname: faker.person.firstName(),
         lastname: faker.person.lastName(),
         email: faker.internet.email(),
-        username: faker.internet.userName()  
-      };
-    
-      await usersCollection.insertOne(sampleUser);
-    
-      const findUser = await usersCollection.findOne({ username: sampleUser.username });
-    
-      expect(findUser.username).toBe(sampleUser.username);
-    }, 30000);
-  
-    // After each test remove all test users
-    afterEach(async () => {
-      await usersCollection.deleteMany({});
+        username: faker.internet.userName(),
+      });
+    }
+
+    const result = await usersCollection.insertMany(newUsers);
+    expect(result.insertedCount).toBe(total_users_to_add);
+  }, 30000);
+
+  // test reading users
+  test("Test READ", async () => {
+    let sampleUser = {
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      username: faker.internet.userName(),
+    };
+
+    await usersCollection.insertOne(sampleUser);
+
+    const findUser = await usersCollection.findOne({
+      username: sampleUser.username,
     });
-  
-    // after all close the client
-    afterAll(async () => {
-      await client.close();
-    });
+
+    expect(findUser.username).toBe(sampleUser.username);
+  }, 30000);
+
+  // After each test remove all test users
+  afterEach(async () => {
+    await usersCollection.deleteMany({});
   });
+
+  // after all close the client
+  afterAll(async () => {
+    await client.close();
+  });
+});
 
 // describe("API Tests", () =>{
 
@@ -93,23 +92,22 @@ describe("Database Tests", () => {
 //     expect(response.statusCode).toBe(200);
 // });
 
+// it("test adding user", (done) => {
+//     let user = {username: faker.internet.userName(), firstname: faker.person.firstName() , lastname: faker.person.lastName(), email: faker.internet.email(), school: "monash", password: faker.internet.password()};
+//     const response = request(app)
+//     .post('/users')
+//     .send()
+//     .set('Accept', 'application/json')
+//     .expect(200)
+//     .expect((res) => {
+//       res.body.username = user.username;
+//     })
+//     .end((err, res) => {
+//       if (err) return done(err);
+//         return done();
+//     })
+// })
 
-  // it("test adding user", (done) => {
-  //     let user = {username: faker.internet.userName(), firstname: faker.person.firstName() , lastname: faker.person.lastName(), email: faker.internet.email(), school: "monash", password: faker.internet.password()};
-  //     const response = request(app)
-  //     .post('/users')
-  //     .send()
-  //     .set('Accept', 'application/json')
-  //     .expect(200)
-  //     .expect((res) => {
-  //       res.body.username = user.username;
-  //     })
-  //     .end((err, res) => {
-  //       if (err) return done(err);
-  //         return done();
-  //     })
-  // })
-  
 //   afterAll(async () => {
 //     await client.close();
 //   });
