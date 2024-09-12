b54a6b01f421f9a15b013337ed3dacbf33b2377c
Max Verhoef <mver0017@student.monash.edu>
2024-09-12 21:57:26 +1000

    feat: updated api provider url

--- client/src/context/ApiProvider.jsx
+++ client/src/context/ApiProvider.jsx
@@ -24,7 +24,7 @@ export const useApi = () => useContext(ApiContext);
 // MongoDB API Provider component
 export const ApiProvider = ({ children }) => {
   // Define your MongoDB API base URL
-  const baseURL = `${process.env.REACT_APP_MONGO_BASE_URL}:5000`;
+  const baseURL = `${process.env.REACT_APP_MONGO_BASE_URL}`;
 
   // Generic function to make API requests
   const fetchData = async (url, options = {}) => {
