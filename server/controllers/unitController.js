8c02499f796cb2ea343e179ba56c693c67637e95
Edward Griffith <edwardgriffith12@gmail.com>
2024-09-29 21:28:00 +1000

    Each handler implemented for controller completion

--- server/controllers/unitController.js
+++ server/controllers/unitController.js
@@ -51,6 +51,250 @@ const getUnits = asyncHandler(async (req, res) => {
   res.status(200).json(units);
 });
 
+/**
+ * @desc    Get a specific unit by ID
+ * @route   GET /api/units/:id
+ * @access  Private
+ * @function getUnit
+ * @async
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>} A promise that resolves on unit retrieval
+ * @throws {Error} Throws a 404 error if the unit  not found.
+ */
+const getUnit = asyncHandler(async (req, res) => {
+  const unitId = req.params.id;
+  const units = await unitModel.find();
+  const unit = units.find((unit) => unit._id == unitId);
+
+  if (!unit) {
+    res.status(404).json({ message: "Unit not found" });
+  } else {
+    res.status(200).json(unit);
+  }
+});
+
+/**
+ * @desc    Create a new unit
+ * @route   POST /api/units
+ * @access  Public
+ * @function createUnit
+ * @async
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>} A promise that resolves when the unit is created successfully.
+ */
+const createUnit = asyncHandler(async (req, res) => {
+  const { title, icon, colour } = req.body;
+
+  const newLesson = await createLesson(); // placeholder lesson
+
+  const newLessonForUnit = {
+    _id: newLesson._id,
+    id: newLesson._id.toString(),
+    icon: "lessonIcon",
+    title: "New Lesson",
+    tooltip: { content: "Description of the new child node" },
+    children: [],
+    type: "lesson",
+  };
+
+  const unitDetails = await unitModel.create({
+    data: [newLessonForUnit],
+    numberOfLessons: 1,
+  });
+
+  const unit = await unitsModel.create({
+    _id: unitDetails._id,
+    title,
+    icon,
+    colour,
+    numberOfLessons: 1,
+  });
+
+  res.status(200).json(unit);
+});
+
+/**
+ * @desc    Add a node to a unit
+ * @route   POST /api/units/:id/append
+ * @access  Private
+ * @function appendNode
+ * @async
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>} A promise that resolves when the node is appended successfully.
+ */
+const appendNode = asyncHandler(async (req, res) => {
+  const { unitId, targetNodeId, newNode, inputSubType } = req.body;
+
+  const unit = await unitModel.findById(unitId);
+  if (!unit) {
+    return res.status(404).json({ message: "Unit not found" });
+  }
+
+  let generatedNode;
+  if (newNode.type === "lesson") {
+    generatedNode = await createLesson(newNode.title, newNode.tooltip.content);
+  } else if (newNode.type === "video") {
+    generatedNode = await createVideo(newNode.title, newNode.tooltip.content);
+  } else if (newNode.type === "quiz") {
+    generatedNode = await createQuiz(newNode.title, inputSubType, newNode.tooltip.content);
+  } else {
+    return res.status(500).json({ message: "Invalid node type" });
+  }
+
+  if (!generatedNode) {
+    return res.status(500).json({ message: "Error creating node" });
+  }
+
+  newNode.id = generatedNode._id.toString(); // Convert ObjectId to string
+
+  const isUpdated = appendChildNode(unit.data, targetNodeId, newNode);
+  if (!isUpdated) {
+    return res.status(404).json({ message: "Target node not found" });
+  }
+
+  unit.numberOfLessons += 1;
+  unit.markModified("data");
+  await unit.save();
+
+  const unitsUpdated = await unitsModel.updateOne(
+    { _id: unitId },
+    { $inc: { numberOfLessons: 1 } }
+  );
+
+  if (!unitsUpdated) {
+    return res.status(404).json({ message: "Units not found" });
+  }
+
+  res.status(200).json({ message: "Node appended successfully", newNode });
+});
+
+/**
+ * @desc    Insert a node into a unit
+ * @route   POST /api/units/:id/insert
+ * @access  Private
+ * @function insertNode
+ * @async
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @returns {Promise<void>} Node inserted successfully.
+ */
+const insertNode = asyncHandler(async (req, res) => {
+  const { unitId, targetNodeId, newNode, inputSubType } = req.body;
+
+  const unit = await unitModel.findById(unitId);
+  if (!unit) {
+    return res.status(404).json({ message: "Unit not found" });
+  }
+
+  let generatedNode;
+  if (newNode.type === "lesson") {
+    generatedNode = await createLesson(newNode.title, newNode.tooltip.content);
+  } else if (newNode.type === "video") {
+    generatedNode = await createVideo(newNode.title, newNode.tooltip.content);
+  } else if (newNode.type === "quiz") {
+    generatedNode = await createQuiz(newNode.title, inputSubType, newNode.tooltip.content);
+  } else {
+    return res.status(500).json({ message: "Invalid node type" });
+  }
+
+  if (!generatedNode) {
+    return res.status(500).json({ message: "Error creating node" });
+  }
+
+  newNode.id = generatedNode._id.toString(); // Convert ObjectId to string
+
+  const isUpdated = insertChildNode(unit.data, targetNodeId, newNode);
+  if (!isUpdated) {
+    return res.status(404).json({ message: "Target node not found" });
+  }
+
+  unit.numberOfLessons += 1;
+  unit.markModified("data");
+  await unit.save();
+
+  const unitsUpdated = await unitsModel.updateOne(
+    { _id: unitId },
+    { $inc: { numberOfLessons: 1 } }
+  );
+
+  if (!unitsUpdated) {
+    return res.status(404).json({ message: "Units not found" });
+  }
+
+  res.status(200).json({ message: "Node inserted successfully", newNode });
+});
+
+/**
+ * @desc    Recursively adds child node to the target's children.
+ * @function appendChildNode
+ * @param {Object[]} data - The tree data to append the node to.
+ * @param {string} targetId - The ID of the target node.
+ * @param {Object} newNode - The new node to append.
+ * @returns {boolean} Returns true if the node is appended, otherwise false.
+ */
+function appendChildNode(data, targetId, newNode) {
+  for (let item of data) {
+    if (item.id === targetId) {
+      item.children.push(newNode);
+      return true;
+    }
+
+    if (item.children && item.children.length > 0) {
+      const found = appendChildNode(item.children, targetId, newNode);
+      if (found) return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * @desc    Recursively insert a child node into the target node's children.
+ * @function insertChildNode
+ * @param {Object[]} data - The tree data to insert the node into.
+ * @param {string} targetId - The ID of the target node.
+ * @param {Object} newNode - new node to insert.
+ * @returns {boolean} True if inserted
+ */
+function insertChildNode(data, targetId, newNode) {
+  for (let item of data) {
+    if (item.id === targetId) {
+      newNode.children = item.children;
+      item.children = [newNode];
+      return true;
+    }
+
+    if (item.children && item.children.length > 0) {
+      const found = insertChildNode(item.children, targetId, newNode);
+      if (found) return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * @desc    Get the list of tail node IDs
+ * @function getTailNodeIds
+ * @param {Object[]} treeData - The tree data to traverse.
+ * @returns {string[]} An array of tail node IDs.
+ */
+function getTailNodeIds(treeData) {
+  const tailNodeIds = [];
+
+  for (const node of treeData) {
+    if (!node.children || node.children.length === 0) {
+      tailNodeIds.push(node.id);
+    } else {
+      const childTailNodeIds = getTailNodeIds(node.children);
+      tailNodeIds.push(...childTailNodeIds);
+    }
+  }
+
+  return tailNodeIds;
+}
+
 module.exports = {
   getUnits,
   getUnit,
