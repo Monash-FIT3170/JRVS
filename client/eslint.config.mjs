e419eb9b7402daa0b698f3eb2d7d8a7635a53610
Max Verhoef <mver0017@student.monash.edu>
2024-10-06 13:47:42 +1100

    feat: disabled react/prop-types in eslint config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -3,7 +3,16 @@ import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
   {
     languageOptions: {
       globals: {
@@ -14,6 +23,4 @@ export default [
       },
     },
   },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
